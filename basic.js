bcbbd2c3e230371b5c1b367c5dada3a7b6322a57
richardykedang <kedangrichard@gmail.com>
2020-12-22 15:48:27 +0700

    socket io fundamental with chat

--- basic.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const socketio = require('socket.io');
-
-const server = http.createServer((req, res) => {
-    res.end('Iam connected');
-});
-
-//testinng server
-server.listen(8000);
